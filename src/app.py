f6bf13f18abf51b657fd890c4386c12649c4d4a4
MaxNguyen5040 <maxnguyen3012@gmail.com>
2024-07-06 16:45:54 -0500

    Added user data saving, refactored code files

--- src/app.py
+++ src/app.py
@@ -4,16 +4,23 @@ from googletrans import Translator
 from textblob import TextBlob
 
 translator = Translator()
-
-
 app = Flask(__name__)
 manager = DialogueManager()
+user_preferences = {}
+
+def update_user_preferences(user_id, key, value):
+    if user_id not in user_preferences:
+        user_preferences[user_id] = {}
+    user_preferences[user_id][key] = value
+
+def get_user_preferences(user_id):
+    return user_preferences.get(user_id, {})
 
 def translate_message(user_message, target_lang='en'):
     translation = translator.translate(user_message, dest=target_lang)
     return translation.text
 
-@app.route('/chat', methods=['POST'])
+@app.route('/lingual_chat', methods=['POST'])
 def mutli_lingual_chat():
     user_message = request.json.get('message')
     translated_message = translate_message(user_message, target_lang='en')
@@ -21,6 +28,7 @@ def mutli_lingual_chat():
     response = handle_intent(intent, entities)
     return jsonify({'response': response})
 
+@app.route('/sentiment_chat', methods=['POST'])
 def sentiment_chat():
     user_message = request.json.get('message')
     translated_message = translate_message(user_message, target_lang='en')
@@ -29,10 +37,19 @@ def sentiment_chat():
     response = handle_intent(intent, entities)
     return jsonify({'response': response, 'sentiment_polarity': sentiment_polarity, 'sentiment_subjectivity': sentiment_subjectivity})
 
-user_message = "¿Cómo está el clima hoy?"
-translated_message = translate_message(user_message, target_lang='en')
-print(translated_message)
 
-user_message = "I am very happy today!"
-polarity, subjectivity = analyze_sentiment(user_message)
-print(f"Polarity: {polarity}, Subjectivity: {subjectivity}")
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_id = request.json.get('user_id')
+    user_message = request.json.get('message')
+    translated_message = translate_message(user_message, target_lang='en')
+    intent, entities = recognize_intent(translated_message)
+    sentiment_polarity, sentiment_subjectivity = analyze_sentiment(translated_message)
+    response = handle_intent(intent, entities)
+    update_user_preferences(user_id, 'last_message', user_message)
+    return jsonify({'response': response, 'sentiment_polarity': sentiment_polarity, 'sentiment_subjectivity': sentiment_subjectivity, 'user_preferences': get_user_preferences(user_id)})
+
+user_id = "user123"
+user_message = "I love sunny weather!"
+update_user_preferences(user_id, 'favorite_weather', 'sunny')
+print(get_user_preferences(user_id))
